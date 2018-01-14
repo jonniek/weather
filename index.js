461f3a45617b2272d7d6beb4f7d3e291e1c131b4
jonniek <kayhty@windowslive.com>
2018-01-14 23:46:32 +0900

    finished menu and graph

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const express = require('express')
 const app = express()
 
-// use http as middleware for express server
-// so that socket io can listen in on events
+// add static folder to load icons and js
+app.use(express.static('static'))
+
 const http = require('http').Server(app)
 
 // use socket io for communication between
@@ -25,31 +26,44 @@ const locations = [
 	{
 		id: 0,
 		name: 'Tokyo',
+		slug: 'tokyo',
 		coordinates: [139.7328635, 35.6584421],
-		topoindex: 82 
+		topoindex: 82,
+		utcOffsetHours: 9
 	}, {
 		id: 1,
 		name: 'Helsinki',
+		slug: 'helsinki',
 		coordinates: [24.9490830, 60.1697530],
-		topoindex: 52
+		topoindex: 52,
+		utcOffsetHours: 2 // TODO in the summer it's 3!!
 	}, {
 		id: 2,
 		name: 'New York',
+		slug: 'newyork',
 		coordinates: [-73.9938438, 40.7406905],
-		topoindex: 168
+		topoindex: 168,
+		utcOffsetHours: -5 // -4 in the summer..
 	}, {
 		id: 3,
 		name: 'Amsterdam',
+		slug: 'amsterdam',
 		coordinates: [4.9040238, 52.3650691],
-		topoindex: 117 
+		topoindex: 117,
+		utcOffsetHours: 1 // I guess I need some api for this..
 	}, {
 		id: 4,
 		name: 'Dubai',
+		slug: 'dubai',
 		coordinates: [55.1562243, 25.092535],
-		topoindex: 3 
+		topoindex: 3,
+		utcOffsetHours: 4
 	}
 ]
-const allLocIds = locations.map(l => l.id)
+
+const locationIdExists = (id) => {
+	return locations.filter(l => l.id === id).length > 0
+}
 
 // setup our database from file
 const temperatureDatabase =
@@ -66,7 +80,7 @@ const saveMeasurement = (data) => {
 	return new Promise((resolve, reject) => {
 		temperatureDatabase.insert({
 			locationId,
-			temperature, // backend stores measuremnt in celcius
+			temperature,
 			timestamp
 		}, (err, doc) => {
 			if(err){
@@ -78,6 +92,7 @@ const saveMeasurement = (data) => {
 	})
 }
 
+// gets all measurements from the past 24 hours
 const getMeasurements = () => {
 	const onedayago = Date.now() - 24*60*60*1000
 	return new Promise((resolve, reject) => {
@@ -93,9 +108,6 @@ const getMeasurements = () => {
 			})
 	})
 }
-
-
-app.use(express.static('static'))
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
@@ -117,19 +129,24 @@ app.get('/data', (req, res) => {
 io.on('connection', (socket) => {
 
 	// when user sends temperature measurements
-	// save it in the database and 
+	// save it in the database and emit to everyone
   socket.on('add', (measurement, callback) => {
 
-  	// verify data
-  	const id = measurement.id
-  	const temp = measurement.temperature
+  	// make sure measurements is not undefined
+  	measurement = measurement === undefined
+  		? {} : measurement
+  	// verify measurements data
+  	const id = +measurement.id
+  	const temp = +measurement.temperature
 
-  	const validId = allLocIds.indexOf(id) !== -1
+  	const validId = locationIdExists(id)
   	const validTemp = temp < 101 && temp > -101
 
   	if (!validId || !validTemp) {
   		// data is corrupted
+  		console.log("corrupted input", measurement)
     	callback({ success: false })
+    	return
   	}
 
     saveMeasurement(measurement)
