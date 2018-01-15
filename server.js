c0fcc12f7b6bb839f54133cca160022e74cbb5ce
jonniek <kayhty@windowslive.com>
2018-01-15 18:08:33 +0900

    add port env variable

--- server.js
+++ server.js
@@ -162,8 +162,9 @@ io.on('connection', (socket) => {
   })
 })
 
+const port = process.env.PORT || 3000
 
-http.listen(3000, () => {
-  console.log('listening on *:3000')
+http.listen(port, () => {
+  console.log('listening on: localhost:' + port)
 })
 
